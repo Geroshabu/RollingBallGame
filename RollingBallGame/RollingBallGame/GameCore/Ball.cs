a5585b2f77e18fa45ef8743befb21cc1b6093bea
Geroshabu <oonstks@gmail.com>
2016-06-08 12:47:17 +0900

    refs #16 Declare operations of Ball

--- RollingBallGame/RollingBallGame/GameCore/Ball.cs
+++ RollingBallGame/RollingBallGame/GameCore/Ball.cs
@@ -1,8 +1,48 @@
 using System;
+using System.Windows;
 
 namespace RollingBallGame.GameCore
 {
+	public struct GridPoint
+	{
+		public int X;
+		public int Y;
+	}
+
 	public class Ball
 	{
+		/// <summary>
+		/// 現在位置
+		/// </summary>
+		public Point Position { get; private set; }
+		/// <summary>
+		/// マス目で数えた場合の位置
+		/// </summary>
+		public GridPoint GridPosition
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		/// <summary>
+		/// 速度
+		/// </summary>
+		public Vector Velocity { get; private set; }
+
+		/// <summary>
+		/// 加速度
+		/// </summary>
+		public Vector Acceleration { get; private set; }
+
+		/// <summary>
+		/// 加速度を与え、ボールを1ターン分移動させる
+		/// </summary>
+		/// <param name="acceleration">ボールに与える加速度</param>
+		public void Roll(Vector acceleration)
+		{
+			throw new NotImplementedException();
+		}
 	}
 }
