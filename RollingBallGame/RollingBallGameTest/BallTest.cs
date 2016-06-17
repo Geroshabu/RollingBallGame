85d42c6aed643fcacca48aff7d0be5c76007f75a
Geroshabu <oonstks@gmail.com>
2016-06-17 18:43:55 +0900

    refs #20 Add tests for Roll method

--- RollingBallGame/RollingBallGameTest/BallTest.cs
+++ RollingBallGame/RollingBallGameTest/BallTest.cs
@@ -22,16 +22,16 @@ namespace RollingBallGameTest
 		{
 			get
 			{
-				yield return new object[] { new Point(3.0, 4.0), new GridPoint(3, 4) };	// ÂÂ®Ââ€(ÂÂ³)
-				yield return new object[] { new Point(2.999, 4.0), new GridPoint(2, 4) };	// XÂÃ€â€¢Wâ€™[Ââ€ÂË†â€”Â(ÂÂ³)
-				yield return new object[] { new Point(3.0, 3.999), new GridPoint(3, 3) };	// YÂÃ€â€¢Wâ€™[Ââ€ÂË†â€”Â(ÂÂ³)
-				yield return new object[] { new Point(-3.0, -4.0), new GridPoint(-3, -4) };	// ÂÂ®Ââ€(â€¢â€°)
-				yield return new object[] { new Point(-3.001, -4.0), new GridPoint(-4, -4) };	// XÂÃ€â€¢Wâ€™[Ââ€ÂË†â€”Â(â€¢â€°)
-				yield return new object[] { new Point(-3.0, -4.001), new GridPoint(-3, -5) };	// YÂÃ€â€¢Wâ€™[Ââ€ÂË†â€”Â(â€¢â€°)
+				yield return new object[] { new Point(3.0, 4.0), new GridPoint(3, 4) };	// ®”(³)
+				yield return new object[] { new Point(2.999, 4.0), new GridPoint(2, 4) };	// XÀ•W’[”ˆ—(³)
+				yield return new object[] { new Point(3.0, 3.999), new GridPoint(3, 3) };	// YÀ•W’[”ˆ—(³)
+				yield return new object[] { new Point(-3.0, -4.0), new GridPoint(-3, -4) };	// ®”(•‰)
+				yield return new object[] { new Point(-3.001, -4.0), new GridPoint(-4, -4) };	// XÀ•W’[”ˆ—(•‰)
+				yield return new object[] { new Point(-3.0, -4.001), new GridPoint(-3, -5) };	// YÀ•W’[”ˆ—(•‰)
 			}
 		}
 
-		[Theory(DisplayName = "GridPointÆ’vÆ’ÂÆ’pÆ’eÆ’Bâ€šÃŒÅ mâ€F")]
+		[Theory(DisplayName = "GridPointƒvƒƒpƒeƒB‚ÌŠm”F")]
 		[MemberData("GridPositionTestData")]
 		public void GridPointTest(Point position, GridPoint expected)
 		{
@@ -42,6 +42,50 @@ namespace RollingBallGameTest
 			property_info.SetValue(ball, position);
 
 			Assert.Equal(expected, ball.GridPosition);
+		}
+
+		public static IEnumerable<object[]> RollTestData
+		{
+			get
+			{
+				yield return new object[] {	// ³‚Ì‰Á‘¬“x‚ğ—^‚¦‚é
+					new Point(0.2, 0.3), new Vector(0.5, 0.7), new Vector(1.1, 1.3), new Vector(1.7, 1.9),
+					new Point(2.4, 2.9), new Vector(2.2, 2.6), new Vector(1.7, 1.9)
+				};
+				yield return new object[] {	// •‰‚Ì‰Á‘¬“x‚ğ—^‚¦‚é. (‚»‚µ‚ÄPosition‚ğ•‰‚É...)
+					new Point(1.7, 1.9), new Vector(0.2, 0.3), new Vector(0.5, 0.7), new Vector(-1.1, -2.3),
+					new Point(0.8, -0.1), new Vector(-0.9, -2.0), new Vector(-1.1, -2.3)
+				};
+
+			}
+		}
+
+		[Theory(DisplayName = "Rollƒƒ\ƒbƒh‚ÌŠm”F")]
+		[MemberData("RollTestData")]
+		public void RollTest(Point position, Vector velocity, Vector acceleration, Vector input,
+							Point expected_position, Vector expected_velocity, Vector expected_acceleration)
+		{
+			Ball ball = new Ball();
+
+			PropertyInfo property_info = ball.GetType().GetProperty("Position",
+				BindingFlags.SetProperty | BindingFlags.Public | BindingFlags.Instance);
+			property_info.SetValue(ball, position);
+			property_info = ball.GetType().GetProperty("Velocity",
+				BindingFlags.SetProperty | BindingFlags.Public | BindingFlags.Instance);
+			property_info.SetValue(ball, velocity);
+			property_info = ball.GetType().GetProperty("Acceleration",
+				BindingFlags.SetProperty | BindingFlags.Public | BindingFlags.Instance);
+			property_info.SetValue(ball, acceleration);
+
+			ball.Roll(input);
+
+			// ŒvZ‚Ì‰ß’ö‚ÅŒë·‚ªo‚é‚Í‚¸. ¸“xw’è‚µ‚Ä”äŠr‚·‚é.
+			Assert.Equal(expected_position.X, ball.Position.X, 1);
+			Assert.Equal(expected_position.Y, ball.Position.Y, 1);
+			Assert.Equal(expected_velocity.X, ball.Velocity.X, 1);
+			Assert.Equal(expected_velocity.Y, ball.Velocity.Y, 1);
+			Assert.Equal(expected_acceleration.X, ball.Acceleration.X, 1);
+			Assert.Equal(expected_acceleration.Y, ball.Acceleration.Y, 1);
 		}
 	}
 }
