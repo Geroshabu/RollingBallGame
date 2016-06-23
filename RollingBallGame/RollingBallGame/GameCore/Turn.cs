44a30c1de1d2bb6726b062712eb1adb7bb79c3b6
Geroshabu <oonstks@gmail.com>
2016-06-23 12:48:54 +0900

    refs #23 Add operations of a Turn

--- RollingBallGame/RollingBallGame/GameCore/Turn.cs
+++ RollingBallGame/RollingBallGame/GameCore/Turn.cs
@@ -4,5 +4,30 @@ namespace RollingBallGame.GameCore
 {
 	public class Turn
 	{
+		public Board Board { get; private set; }
+		/// <summary>
+		/// このターンの実行前のボールの状態.
+		/// </summary>
+		public Ball BallBeforeExecute { get; private set; }
+		/// <summary>
+		/// このターンの実行後のボールの状態.
+		/// 実行する前はnull.
+		/// </summary>
+		/// <seealso cref="Execute"/>
+		public Ball BallAfterExecute { get; private set; }
+
+		public Turn(Board board, Ball ball)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// このターンを実行する.
+		/// 実行後は, <see cref="BallAfterExecute"/>から実行後のボールの状態が取得可能.
+		/// </summary>
+		public void Execute()
+		{
+			throw new NotImplementedException();
+		}
 	}
 }
